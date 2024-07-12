b388b303f524eca34752feb296d9100451b92b45
Sadid <66120003+sadid369@users.noreply.github.com>
2024-07-13 00:42:28 +0600

    updated

--- resources/views/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout');
-
-@section('title')
-    About
-@endsection
-@section('content')
-    <h1>About Page</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,13 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
-
-
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(PageController::class)->group(function () {
+Route::get('/','showHome')->name('home');
+Route::get('/user/{id}','showUser')->name('users');
+Route::get('/blog','showBlog')->name('blog');
 });
 
-Route::get('/post', function () {
-    return view('post');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/test', function () {
-    return view('test');
-});
-
-
-
-
-
+Route::get('/test', TestController::class);
